042eb5db29a545431bebe6a13b1a3f36d04ef4e7
Vlad Ulyanovskiy <ulyanovskiy.2001@mail.ru>
2025-09-12 20:41:10 +0300

    feat: implement connection manager contract

---
+++ WebSocket.Otp.Abstractions/Contracts/IWebSocketConnectionManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.WebSockets;
+
+namespace WebSockets.Otp.Abstractions.Contracts;
+
+public interface IWebSocketConnectionManager
+{
+    string Add(WebSocket webSocket);
+
+    bool Remove(string connectionId);
+
+    WebSocket Get(string connectionId);
+
+    string GetId(WebSocket webSocket);
+
+    IEnumerable<WebSocket> GetAll();
+}
