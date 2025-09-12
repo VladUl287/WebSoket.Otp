a2313bedf9cc2c1f98d11465719506f661795edc
Vlad Ulyanovskiy <ulyanovskiy.2001@mail.ru>
2025-09-12 20:50:16 +0300

    feat: implement registry for endpoints

---
+++ WebSocket.Otp.Abstractions/Contracts/IWebSocketEndpointRegistry.cs
@@ -0,0 +1,10 @@
+﻿namespace WebSockets.Otp.Abstractions.Contracts;
+
+public interface IWebSocketEndpointRegistry
+{
+    void RegisterEndpoint<TEndpoint>(string pattern) where TEndpoint : Endpoint;
+
+    void RegisterEndpoint<TEndpoint, TRequest>(string pattern)
+        where TRequest : class
+        where TEndpoint : Endpoint<TRequest>;
+}
