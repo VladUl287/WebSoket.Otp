4a94578f88160ac4e4ba4b526e34cc3e94325b93
User <ulyanovskiy.2001@mail.ru>
2025-09-17 13:45:06 +0300

    feat: implement basic IdProvider

---
+++ WebSockets.Otp.Core/GuidIdProvider.cs
@@ -0,0 +1,8 @@
+﻿using WebSockets.Otp.Abstractions.Contracts;
+
+namespace WebSockets.Otp.Core;
+
+public sealed class GuidIdProvider : IIdProvider
+{
+    public string NewId() => Guid.NewGuid().ToString();
+}
