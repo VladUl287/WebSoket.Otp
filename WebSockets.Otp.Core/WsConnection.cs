db525c91df3879156a44193aeaa2e21a925d3c1e
User <ulyanovskiy.2001@mail.ru>
2025-09-17 13:44:13 +0300

    feat: implement wsconnection

---
+++ WebSockets.Otp.Core/WsConnection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Security.Claims;
+using WebSockets.Otp.Abstractions.Contracts;
+
+namespace WebSockets.Otp.Core;
+
+public sealed class WsConnection(string id, WebSocket socket, HttpContext context) : IWsConnection
+{
+    public string Id => id;
+
+    public HttpContext? Context => context;
+
+    public WebSocket? Transport => socket;
+
+    public ClaimsPrincipal? User => context.User;
+
+    public string Path => context.Request.Path;
+
+    public string? SubProtocol => socket.SubProtocol;
+
+    public IDictionary<string, object> Items => new ConcurrentDictionary<string, object>();
+
+    public ValueTask SendAsync(ReadOnlyMemory<byte> payload, WebSocketMessageType type, CancellationToken token) =>
+        socket.SendAsync(payload, type, true, token);
+
+    public Task CloseAsync(WebSocketCloseStatus status, string? description, CancellationToken token) =>
+        socket.CloseAsync(status, description, token);
+
+    public ValueTask DisposeAsync()
+    {
+        socket.Dispose();
+        return ValueTask.CompletedTask;
+    }
+}
