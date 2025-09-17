97b5d8c48d1e9467aee18481547170d2eccca2ac
User <ulyanovskiy.2001@mail.ru>
2025-09-17 13:46:01 +0300

    feat: implement inmemort  connection manager

---
+++ WebSockets.Otp.Core/InMemoryConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using WebSockets.Otp.Abstractions.Contracts;
+
+namespace WebSockets.Otp.Core;
+
+public sealed class InMemoryConnectionManager : IWsConnectionManager
+{
+    private readonly ConcurrentDictionary<string, IWsConnection> map = new();
+
+    public bool TryAdd(IWsConnection connection) => map.TryAdd(connection.Id, connection);
+
+    public IWsConnection Get(string connectionId) => map[connectionId];
+
+    public IEnumerable<IWsConnection> GetAll() => map.Values;
+
+    public bool TryRemove(string connectionId) => map.TryRemove(connectionId, out _);
+}
