999266ba55d0d4173b1bf9fb88e903c6dc83f075
User <ulyanovskiy.2001@mail.ru>
2025-09-17 13:37:13 +0300

    feat: add not generic deserialize support

--- WebSocket.Otp.Abstractions/Contracts/IMessageSerializer.cs
+++ WebSocket.Otp.Abstractions/Contracts/IMessageSerializer.cs
@@ -5,4 +5,5 @@ public interface IMessageSerializer
     ReadOnlyMemory<byte> Serialize<T>(T message) where T : IWsMessage;
     T? Deserialize<T>(ReadOnlyMemory<byte> payload) where T : class, IWsMessage;
     string PeekRoute(ReadOnlyMemory<byte> payload);
+    object Deserialize(Type type, ReadOnlyMemory<byte> payload);
 }
