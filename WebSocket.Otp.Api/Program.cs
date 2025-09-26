1cf4dc6e7f74722390c5511cec2661854118a7ed
User <ulyanovskiy.2001@mail.ru>
2025-09-26 14:53:29 +0300

    feat: create middleware

--- WebSocket.Otp.Api/Program.cs
+++ WebSocket.Otp.Api/Program.cs
@@ -1,7 +1,5 @@
 using System.Reflection;
-using WebSockets.Otp.Abstractions.Contracts;
 using WebSockets.Otp.Core;
-using WebSockets.Otp.Core.Extensions;
 using WebSockets.Otp.AspNet.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,63 +27,6 @@ var app = builder.Build();
     app.UseOtpWebSockets((opt) =>
     {
         opt.Path = "/ws";
-    });
-
-    app.Map("/ws", async (HttpContext ctx) =>
-    {
-        if (!ctx.WebSockets.IsWebSocketRequest)
-        {
-            ctx.Response.StatusCode = 400;
-            return;
-        }
-
-        var socket = await ctx.WebSockets.AcceptWebSocketAsync();
-        var conn = new WsConnection(Guid.NewGuid().ToString(), socket, ctx);
-
-        var mgr = ctx.RequestServices.GetRequiredService<IWsConnectionManager>();
-        if (!mgr.TryAdd(conn))
-        {
-            ctx.Response.StatusCode = 400;
-            return;
-        }
-
-        var dispatcher = ctx.RequestServices.GetRequiredService<IMessageDispatcher>();
-        var serializer = ctx.RequestServices.GetRequiredService<IMessageSerializer>();
-        var buffer = new byte[8 * 1024];
-        var ms = new MemoryStream();
-        try
-        {
-            var token = ctx.RequestAborted;
-            while (!token.IsCancellationRequested && socket.State == System.Net.WebSockets.WebSocketState.Open)
-            {
-                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), token);
-                if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Close)
-                {
-                    await conn.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, "closing", CancellationToken.None);
-                    break;
-                }
-
-                if (result.Count > 0)
-                {
-                    ms.Write(buffer, 0, result.Count);
-                }
-                if (result.EndOfMessage)
-                {
-                    var payload = ms.ToArray();
-                    ms.SetLength(0);
-                    await dispatcher.DispatchMessage(conn.AsPublicContext(), payload, token);
-                }
-            }
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-        finally
-        {
-            mgr.TryRemove(conn.Id);
-            await conn.DisposeAsync();
-        }
     });
 
     app.Run();
--- WebSockets.Otp.AspNet/Middlewares/WsMiddleware.cs
+++ WebSockets.Otp.AspNet/Middlewares/WsMiddleware.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Buffers;
+using WebSockets.Otp.Abstractions.Contracts;
 using WebSockets.Otp.AspNet.Options;
+using WebSockets.Otp.Core;
+using WebSockets.Otp.Core.Extensions;
 
 namespace WebSockets.Otp.AspNet.Middlewares;
 
@@ -7,6 +12,68 @@ public sealed class WsMiddleware(RequestDelegate next, WsMiddlewareOptions optio
 {
     public async Task InvokeAsync(HttpContext context)
     {
+        if (context.WebSockets.IsWebSocketRequest && Appropriate(context, options))
+        {
+            await ExecuteWebSocket(context);
+            return;
+        }
+
         await next(context);
+    }
+
+    private static bool Appropriate(HttpContext context, WsMiddlewareOptions options) => context.Request.Path.Equals(options.Path);
+
+    private static async Task ExecuteWebSocket(HttpContext context)
+    {
+        var socket = await context.WebSockets.AcceptWebSocketAsync();
+
+        var idProvider = context.RequestServices.GetRequiredService<IIdProvider>();
+        var wsConnection = new WsConnection(idProvider.NewId(), socket, context);
+
+        var manager = context.RequestServices.GetRequiredService<IWsConnectionManager>();
+        if (!manager.TryAdd(wsConnection))
+        {
+            context.Response.StatusCode = 400;
+            return;
+        }
+
+        var dispatcher = context.RequestServices.GetRequiredService<IMessageDispatcher>();
+        var buffer = ArrayPool<byte>.Shared.Rent(8 * 1024);
+        var ms = new MemoryStream();
+        try
+        {
+            var token = context.RequestAborted;
+            while (!token.IsCancellationRequested && socket.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+                if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Close)
+                {
+                    await wsConnection.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, "closing", CancellationToken.None);
+                    break;
+                }
+
+                if (result.Count > 0)
+                {
+                    ms.Write(buffer, 0, result.Count);
+                }
+
+                if (result.EndOfMessage)
+                {
+                    var payload = ms.ToArray();
+                    ms.SetLength(0);
+                    await dispatcher.DispatchMessage(wsConnection.AsPublicContext(), payload, token);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+            manager.TryRemove(wsConnection.Id);
+            await wsConnection.DisposeAsync();
+        }
     }
 }
