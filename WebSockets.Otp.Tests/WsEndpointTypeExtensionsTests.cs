7aeda3adc4f233303dfddf132c1bab5d25886fff
User <ulyanovskiy.2001@mail.ru>
2025-10-06 22:13:32 +0300

    feat: add tests project

---
+++ WebSockets.Otp.Tests/WsEndpointTypeExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿using WebSockets.Otp.Abstractions;
+using WebSockets.Otp.AspNet.Extensions;
+
+namespace WebSockets.Otp.Tests;
+
+public sealed class WsEndpointTypeExtensionsTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var test = typeof(WsEndpoint).IsWsEndpoint();
+    }
+}
