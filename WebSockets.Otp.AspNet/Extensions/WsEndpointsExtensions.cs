74553ec9a195f860bb58176013cb57a8aa3198e4
User <ulyanovskiy.2001@mail.ru>
2025-10-06 22:38:57 +0300

    feat: implement ws endpoint extensions methods

--- WebSockets.Otp.AspNet/Extensions/WsEndpointsExtensions.cs
+++ WebSockets.Otp.AspNet/Extensions/WsEndpointsExtensions.cs
@@ -1,15 +1,28 @@
-﻿namespace WebSockets.Otp.AspNet.Extensions;
+﻿using System.Runtime.CompilerServices;
+using WebSockets.Otp.Abstractions;
+using WebSockets.Otp.Abstractions.Contracts;
+
+namespace WebSockets.Otp.AspNet.Extensions;
 
 public static class WsEndpointsExtensions
 {
-    public static bool IsWsEndpoint(this Type type)
-    {
-        return true;
-    }    
-    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsWsEndpoint(this Type type) => type.IsAssignableTo(typeof(IWsEndpoint));
+
     public static bool AcceptsRequestMessages(this Type type)
     {
-        return true;
+        if (!type.IsWsEndpoint())
+            return false;
+
+        while (type is not null)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(WsEndpoint<>))
+                return true;
+
+            type = type.BaseType;
+        }
+
+        return false;
     }
 
     public static Type GetRequestType(this Type type)
