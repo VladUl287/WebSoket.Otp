3d1b2b19fe21cc67b5ee2c9d3fcaa14dd6c25a6c
User <ulyanovskiy.2001@mail.ru>
2025-10-06 13:50:38 +0300

    feat: create default path ws request matcher

---
+++ WebSockets.Otp.AspNet/PathWsRequestMatcher.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using WebSockets.Otp.Abstractions.Contracts;
+using WebSockets.Otp.Core.Helpers;
+
+namespace WebSockets.Otp.AspNet;
+
+public sealed class PathWsRequestMatcher(string path) : IWsRequestMatcher
+{
+    public bool IsWebSocketRequest(HttpContext context)
+    {
+        return context.WebSockets.IsWebSocketRequest && PathHelper.Normilize(context.Request.Path).Equals(path);
+    }
+}
