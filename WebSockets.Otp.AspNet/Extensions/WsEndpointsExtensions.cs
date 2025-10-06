261f8340036f92cd989186b6f5872032b07e4710
User <ulyanovskiy.2001@mail.ru>
2025-10-06 17:33:24 +0300

    feat: create ws endpoints type extensions

---
+++ WebSockets.Otp.AspNet/Extensions/WsEndpointsExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace WebSockets.Otp.AspNet.Extensions;
+
+public static class WsEndpointsExtensions
+{
+    public static bool IsWsEndpoint(this Type type)
+    {
+        return true;
+    }    
+    
+    public static bool AcceptsRequestMessages(this Type type)
+    {
+        return true;
+    }
+
+    public static Type GetRequestType(this Type type)
+    {
+        return type;
+    }
+}
