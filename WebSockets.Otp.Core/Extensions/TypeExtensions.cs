faf493471824baa13f2f6fd3d9883ec52ad545f1
User <ulyanovskiy.2001@mail.ru>
2025-10-07 10:59:25 +0300

    feat: remove unused extension class

--- WebSockets.Otp.Core/Extensions/TypeExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebSockets.Otp.Core.Extensions;
-
-public static class TypeExtensions
-{
-    public static bool ImplementsInterface<TInterface>(this Type type) => type is not null && typeof(TInterface).IsAssignableFrom(type);
-}
