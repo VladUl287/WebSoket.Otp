489938800a37039201abed925295c11e185d9683
User <ulyanovskiy.2001@mail.ru>
2025-10-08 14:52:08 +0300

    feat: create native chunk buffer for reduce gc pressure on big message processing

---
+++ WebSockets.Otp.Core/Helpers/NativeChunkBuffer.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSockets.Otp.Core.Helpers;
+
+public sealed class NativeChunkBuffer : IDisposable
+{
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    ~NativeChunkBuffer()
+    {
+        Dispose();
+    }
+}
