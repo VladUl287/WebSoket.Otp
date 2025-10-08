65f062623b025e088e542921700496ad2c4633c6
User <ulyanovskiy.2001@mail.ru>
2025-10-08 16:15:49 +0300

    feat: implement shrink and clear methods

--- WebSockets.Otp.Core/Helpers/NativeChunkBuffer.cs
+++ WebSockets.Otp.Core/Helpers/NativeChunkBuffer.cs
@@ -1,27 +1,55 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace WebSockets.Otp.Core.Helpers;
 
 public sealed unsafe class NativeChunkBuffer : IDisposable
 {
-    private readonly byte* _buffer;
+    private byte* _buffer;
+
     private readonly int _initialCapacity;
+    private int _capacity;
+    private int _position;
     private bool _disposed;
 
     public NativeChunkBuffer(int capacity)
     {
         _initialCapacity = capacity;
+        _capacity = capacity;
         _buffer = (byte*)NativeMemory.AllocZeroed((uint)capacity);
     }
 
     public void Write(ReadOnlySpan<byte> data)
     {
-
+        ThrowIfDisposed();
     }
 
     public void Shrink()
     {
+        ThrowIfDisposed();
+
+        if (_capacity <= _initialCapacity)
+            return;
+
+        void* newPtr = NativeMemory.Realloc(_buffer, (uint)_initialCapacity);
+        if (newPtr is null)
+            throw new OutOfMemoryException("Realloc failed.");
+
+        _buffer = (byte*)newPtr;
+        _capacity = _initialCapacity;
+        _position = _initialCapacity;
     }
+
+    public void Clear()
+    {
+        ThrowIfDisposed();
+
+        NativeMemory.Clear(_buffer, (uint)_capacity);
+        _position = 0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void ThrowIfDisposed() => ObjectDisposedException.ThrowIf(_disposed, "Native chunk buffer already disposed");
 
     public void Dispose()
     {
