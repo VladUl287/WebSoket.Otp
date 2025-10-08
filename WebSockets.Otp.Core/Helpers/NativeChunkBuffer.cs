53b6c86459076191bff295592115f90913cd3a95
User <ulyanovskiy.2001@mail.ru>
2025-10-08 15:05:16 +0300

    feat: add native chunk buffer base public methods

--- WebSockets.Otp.Core/Helpers/NativeChunkBuffer.cs
+++ WebSockets.Otp.Core/Helpers/NativeChunkBuffer.cs
@@ -1,9 +1,34 @@
-﻿namespace WebSockets.Otp.Core.Helpers;
+﻿using System.Runtime.InteropServices;
 
-public sealed class NativeChunkBuffer : IDisposable
+namespace WebSockets.Otp.Core.Helpers;
+
+public sealed unsafe class NativeChunkBuffer : IDisposable
 {
+    private readonly byte* _buffer;
+    private readonly int _initialCapacity;
+    private bool _disposed;
+
+    public NativeChunkBuffer(int capacity)
+    {
+        _initialCapacity = capacity;
+        _buffer = (byte*)NativeMemory.AllocZeroed((uint)capacity);
+    }
+
+    public void Write(ReadOnlySpan<byte> data)
+    {
+
+    }
+
+    public void Shrink()
+    {
+    }
+
     public void Dispose()
     {
+        if (_disposed) return;
+        _disposed = true;
+
+        NativeMemory.Free(_buffer);
         GC.SuppressFinalize(this);
     }
 
