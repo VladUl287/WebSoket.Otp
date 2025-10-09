d8eafa52bf6c58e672f0fd8eedfe3ea103913f3e
User <ulyanovskiy.2001@mail.ru>
2025-10-09 16:30:00 +0300

    feat: create message bufffer

---
+++ WebSocket.Otp.Abstractions/IMessageBuffer.cs
@@ -0,0 +1,9 @@
+﻿using System.Buffers;
+
+namespace WebSockets.Otp.Abstractions;
+
+public interface IMessageBuffer : IDisposable
+{
+    ReadOnlySpan<byte> Span { get; }
+    IMemoryOwner<byte> Manager { get; }
+}
