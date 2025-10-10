45aa0c0587b259b32ed9b3ce7e994b7a4c3da69d
User <ulyanovskiy.2001@mail.ru>
2025-10-10 22:03:52 +0300

    feat: update request matcher

--- WebSockets.Otp.AspNet/PathWsRequestMatcher.cs
+++ WebSockets.Otp.AspNet/PathWsRequestMatcher.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using WebSockets.Otp.Abstractions.Contracts;
-using WebSockets.Otp.Core.Helpers;
 
 namespace WebSockets.Otp.AspNet;
 
 public sealed class PathWsRequestMatcher(string path) : IWsRequestMatcher
 {
-    public bool IsWebSocketRequest(HttpContext context)
-    {
-        return context.WebSockets.IsWebSocketRequest;
-        //return context.WebSockets.IsWebSocketRequest && PathHelper.Normilize(context.Request.Path).Equals(path);
-    }
+    public bool IsWebSocketRequest(HttpContext context) =>
+        context.WebSockets.IsWebSocketRequest && context.Request.Path.Equals(path);
 }
