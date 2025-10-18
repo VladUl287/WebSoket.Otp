8f22fdc5abf4b2cf1f368b128591d90c68ba410c
User <ulyanovskiy.2001@mail.ru>
2025-10-18 15:56:49 +0300

    feat: make onconnected and ondisconnected steps async support

---
+++ WebSocket.Otp.Abstractions/Options/WsMiddlewareOptions.cs
@@ -0,0 +1,19 @@
+﻿using WebSockets.Otp.Abstractions.Contracts;
+
+namespace WebSockets.Otp.Abstractions.Options;
+
+public sealed class WsMiddlewareOptions
+{
+    public string RequestPath { get; set; } = string.Empty;
+
+    public int MaxMessageSize { get; set; } = 64 * 1024; //64kb
+
+    public int InitialBufferSize { get; set; } = 8 * 1024; // 8KB
+
+    public bool ReclaimBufferAfterEachMessage { get; set; } = true;
+
+    public IWsRequestMatcher RequestMatcher { get; set; } = default!;
+
+    public Func<IWsConnection, Task>? OnConnected { get; set; }
+    public Func<IWsConnection, Task>? OnDisconnected { get; set; }
+}
