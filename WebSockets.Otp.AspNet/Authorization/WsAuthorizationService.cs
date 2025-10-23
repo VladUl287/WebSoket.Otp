183f9567576ea2a54af67521a0f88e4e464f0b27
User <ulyanovskiy.2001@mail.ru>
2025-10-23 11:24:16 +0300

    feat: ws authorization service logging

--- WebSockets.Otp.AspNet/Authorization/WsAuthorizationService.cs
+++ WebSockets.Otp.AspNet/Authorization/WsAuthorizationService.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using WebSockets.Otp.Abstractions;
 using WebSockets.Otp.Abstractions.Options;
 using WebSockets.Otp.Abstractions.Results;
+using WebSockets.Otp.AspNet.Logging;
 
 namespace WebSockets.Otp.AspNet.Authorization;
 
-public class WsAuthorizationService(IAuthorizationService authorizationService) : IWsAuthorizationService
+public class WsAuthorizationService(IAuthorizationService authorizationService, ILogger<WsAuthorizationService> logger) : IWsAuthorizationService
 {
     public virtual async Task<WsAuthorizationResult> AuhtorizeAsync(HttpContext context, WsAuthorizationOptions options)
     {
         if (options is null or { RequireAuthorization: false })
+        {
+            logger.LogAuthorizationNotRequired();
             return WsAuthorizationResult.Success();
+        }
 
         if (authorizationService is null)
-            return WsAuthorizationResult.Failure("Authorization service not presented");
+        {
+            logger.LogAuthorizationServiceMissing();
+            return WsAuthorizationResult.Failure("Authorization service not provided");
+        }
 
         if (context is { User.Identity.IsAuthenticated: false })
-            return WsAuthorizationResult.Failure("User not authenticated");
+        {
+            logger.LogUserNotAuthenticated();
+            return WsAuthorizationResult.Failure("User is not authenticated");
+        }
 
         if (options is { Policies.Length: > 0 })
         {
@@ -26,19 +37,27 @@ public class WsAuthorizationService(IAuthorizationService authorizationService) 
                 var policyAuthResult = await authorizationService.AuthorizeAsync(context.User, policy);
                 if (policyAuthResult.Failure is not null)
                 {
-                    var failureReason = policyAuthResult.Failure.FailureReasons.FirstOrDefault()?.Message ?? $"Policy: '{policy}' unknown authorization failure";
+                    var failureReason = policyAuthResult.Failure.FailureReasons.FirstOrDefault()?.Message ?? $"Policy '{policy}': unknown authorization failure";
+                    logger.LogPolicyAuthorizationFailed(policy, failureReason);
                     return WsAuthorizationResult.Failure(failureReason);
                 }
             }
         }
 
         if (options.Roles is { Length: > 0 } && !options.Roles.Any(context.User.IsInRole))
-            return WsAuthorizationResult.Failure("User not in required role");
+        {
+            logger.LogUserNotInRequiredRole();
+            return WsAuthorizationResult.Failure("User is not in the required role");
+        }
 
         var authScheme = context.User.Identity?.AuthenticationType;
         if (options is { Schemes.Length: > 0 } && !options.Schemes.Contains(authScheme))
-            return WsAuthorizationResult.Failure("User authorization scheme not correct");
+        {
+            logger.LogInvalidAuthorizationScheme();
+            return WsAuthorizationResult.Failure("User authorization scheme is not valid");
+        }
 
+        logger.LogAuthorizationSucceeded();
         return WsAuthorizationResult.Success();
     }
 }
