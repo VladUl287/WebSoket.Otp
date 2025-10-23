52187fe423fee25355c4b64db91f480ff0cdb36e
User <ulyanovskiy.2001@mail.ru>
2025-10-23 11:16:31 +0300

    feat: eliminate auth validators

--- WebSockets.Otp.AspNet/Authorization/WsAuthorizationService.cs
+++ WebSockets.Otp.AspNet/Authorization/WsAuthorizationService.cs
@@ -1,23 +1,43 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using WebSockets.Otp.Abstractions;
 using WebSockets.Otp.Abstractions.Options;
 using WebSockets.Otp.Abstractions.Results;
 
 namespace WebSockets.Otp.AspNet.Authorization;
 
-public sealed class WsAuthorizationService(IEnumerable<IWsAuthorizationValidator> validators) : IWsAuthorizationService
+public class WsAuthorizationService(IAuthorizationService authorizationService) : IWsAuthorizationService
 {
-    public async Task<WsAuthorizationResult> AuhtorizeAsync(HttpContext context, WsAuthorizationOptions options)
+    public virtual async Task<WsAuthorizationResult> AuhtorizeAsync(HttpContext context, WsAuthorizationOptions options)
     {
         if (options is null or { RequireAuthorization: false })
             return WsAuthorizationResult.Success();
 
-        foreach (var validator in validators)
+        if (authorizationService is null)
+            return WsAuthorizationResult.Failure("Authorization service not presented");
+
+        if (context is { User.Identity.IsAuthenticated: false })
+            return WsAuthorizationResult.Failure("User not authenticated");
+
+        if (options is { Policies.Length: > 0 })
         {
-            var result = await validator.ValidateAsync(context, options);
-            if (result.Failed)
-                return result;
+            foreach (var policy in options.Policies ?? [])
+            {
+                var policyAuthResult = await authorizationService.AuthorizeAsync(context.User, policy);
+                if (policyAuthResult.Failure is not null)
+                {
+                    var failureReason = policyAuthResult.Failure.FailureReasons.FirstOrDefault()?.Message ?? $"Policy: '{policy}' unknown authorization failure";
+                    return WsAuthorizationResult.Failure(failureReason);
+                }
+            }
         }
+
+        if (options.Roles is { Length: > 0 } && !options.Roles.Any(context.User.IsInRole))
+            return WsAuthorizationResult.Failure("User not in required role");
+
+        var authScheme = context.User.Identity?.AuthenticationType;
+        if (options is { Schemes.Length: > 0 } && !options.Schemes.Contains(authScheme))
+            return WsAuthorizationResult.Failure("User authorization scheme not correct");
 
         return WsAuthorizationResult.Success();
     }
