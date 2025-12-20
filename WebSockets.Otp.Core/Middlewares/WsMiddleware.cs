83f65d404bf5a59064342a917887c7c24c0068ba
User <ulyanovskiy.2001@mail.ru>
2025-12-20 23:20:56 +0300

    refactor: not wrap middleware exception

--- WebSockets.Otp.Core/Middlewares/WsMiddleware.cs
+++ WebSockets.Otp.Core/Middlewares/WsMiddleware.cs
@@ -1,31 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using WebSockets.Otp.Abstractions.Contracts;
 using WebSockets.Otp.Abstractions.Options;
-using WebSockets.Otp.Core.Logging;
+using WebSockets.Otp.Abstractions.Contracts;
 
 namespace WebSockets.Otp.Core.Middlewares;
 
 public sealed class WsMiddleware(
-    RequestDelegate next, IHandshakeRequestProcessor handshakeProcessor, IWsRequestProcessor wsProcessor,
-    ILogger<WsMiddleware> logger, WsMiddlewareOptions options)
+    RequestDelegate next, IHandshakeRequestProcessor handshakeProcessor, IWsRequestProcessor wsProcessor, 
+    WsMiddlewareOptions options)
 {
     public Task InvokeAsync(HttpContext context)
     {
-        try
-        {
-            if (handshakeProcessor.IsHandshakeRequest(context, options))
-                return handshakeProcessor.HandleRequestAsync(context, options);
+        if (handshakeProcessor.IsHandshakeRequest(context, options))
+            return handshakeProcessor.HandleRequestAsync(context, options);
 
-            if (wsProcessor.IsWebSocketRequest(context, options))
-                return wsProcessor.HandleWebSocketRequestAsync(context, options);
+        if (wsProcessor.IsWebSocketRequest(context, options))
+            return wsProcessor.HandleWebSocketRequestAsync(context, options);
 
-            return next(context);
-        }
-        catch (Exception ex)
-        {
-            logger.WsMiddlewareError(ex);
-            throw;
-        }
+        return next(context);
     }
 }
