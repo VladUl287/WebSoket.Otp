892087cd05e7ea3a8a333337794e75080433c007
User <ulyanovskiy.2001@mail.ru>
2026-01-16 21:52:17 +0300

    feat: make send manager as class

--- WebSocket.Otp.Abstractions/SendManager.cs
+++ WebSocket.Otp.Abstractions/SendManager.cs
@@ -3,49 +3,50 @@ using WebSockets.Otp.Abstractions.Contracts;
 
 namespace WebSockets.Otp.Abstractions;
 
-public readonly struct SendManager(
-   IWsConnectionManager manager,
-   ImmutableArray<string> connectionIds,
-   ImmutableArray<string> groups,
-   bool targetAll)
+public sealed record SendManager(
+   IWsConnectionManager Manager,
+   ImmutableArray<string> ConnectionIds,
+   ImmutableArray<string> Groups,
+   bool TargetAll)
 {
-    private readonly IWsConnectionManager _manager = manager;
-    private readonly ImmutableArray<string> _connectionIds = connectionIds;
-    private readonly ImmutableArray<string> _groups = groups;
-    private readonly bool _targetAll = targetAll;
+    private readonly IWsConnectionManager _manager = Manager;
+    private readonly ImmutableArray<string> _connectionIds = ConnectionIds;
+    private readonly ImmutableArray<string> _groups = Groups;
+    private readonly bool _targetAll = TargetAll;
 
-    public readonly SendManager Client(string connectionId)
+    public SendManager Client(string connectionId)
     {
         if (_targetAll) return this;
 
         return new(_manager, _connectionIds.Add(connectionId), _groups, _targetAll);
     }
 
-    public readonly SendManager Group(string groupName)
+    public SendManager Group(string groupName)
     {
         if (_targetAll) return this;
 
         return new(_manager, _connectionIds, _groups.Add(groupName), _targetAll);
     }
 
-    public readonly SendManager All => new(_manager, _connectionIds, _groups, true);
+    public SendManager All => new(_manager, _connectionIds, _groups, true);
 
-    public readonly ValueTask SendAsync<TResponse>(TResponse data, CancellationToken token)
+    public ValueTask SendAsync<TResponse>(TResponse data, CancellationToken token)
     {
         return ValueTask.CompletedTask;
     }
 }
 
-public readonly struct SendManager<TResponse>(SendManager manager) where TResponse : notnull
+public sealed record SendManager<TResponse>(SendManager Manager) 
+    where TResponse : notnull
 {
-    public readonly SendManager<TResponse> Client(string connectionId) =>
-        new(manager.Client(connectionId));
+    public SendManager<TResponse> Client(string connectionId) =>
+        new(Manager.Client(connectionId));
 
-    public readonly SendManager<TResponse> Group(string groupName) => 
-        new(manager.Group(groupName));
+    public SendManager<TResponse> Group(string groupName) => 
+        new(Manager.Group(groupName));
 
-    public readonly SendManager<TResponse> All => new(manager.All);
+    public SendManager<TResponse> All => new(Manager.All);
 
-    public readonly ValueTask SendAsync(TResponse data, CancellationToken token) =>
-        manager.SendAsync(data, token);
+    public ValueTask SendAsync(TResponse data, CancellationToken token) =>
+        Manager.SendAsync(data, token);
 }
