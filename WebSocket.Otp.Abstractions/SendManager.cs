1a7ab6177de15e62edc3b085f2fa7a1daee19873
User <ulyanovskiy.2001@mail.ru>
2026-01-16 18:11:56 +0300

    feat: extend send manager with connectionmanager

--- WebSocket.Otp.Abstractions/SendManager.cs
+++ WebSocket.Otp.Abstractions/SendManager.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections.Immutable;
+using WebSockets.Otp.Abstractions.Contracts;
 
 namespace WebSockets.Otp.Abstractions;
 
-public readonly struct SendManager<TResponse> 
+public readonly struct SendManager<TResponse>
     where TResponse : notnull
 {
+    private readonly IWsConnectionManager _manager;
     private readonly ImmutableArray<string> _connectionIds;
     private readonly ImmutableArray<string> _groups;
     private readonly bool _targetAll;
 
     private SendManager(
+       IWsConnectionManager manager,
        ImmutableArray<string> connectionIds,
        ImmutableArray<string> groups,
        bool targetAll)
     {
+        _manager = manager;
         _groups = groups;
         _targetAll = targetAll;
         _connectionIds = connectionIds;
@@ -25,7 +29,20 @@ public readonly struct SendManager<TResponse>
             return this;
 
         return new(
+            _manager,
             _connectionIds.Add(connectionId),
+            _groups,
+            _targetAll);
+    }
+
+    public readonly SendManager<TResponse> Client(params IEnumerable<string> connections)
+    {
+        if (_targetAll)
+            return this;
+
+        return new(
+            _manager,
+            _connectionIds.AddRange(connections),
             _groups,
             _targetAll);
     }
@@ -36,12 +53,25 @@ public readonly struct SendManager<TResponse>
             return this;
 
         return new(
+            _manager,
             _connectionIds,
             _groups.Add(groupName),
             _targetAll);
     }
 
-    public readonly SendManager<TResponse> All => new(_connectionIds, _groups, true);
+    public readonly SendManager<TResponse> Group(params IEnumerable<string> groups)
+    {
+        if (_targetAll)
+            return this;
+
+        return new(
+            _manager,
+            _connectionIds,
+            _groups.AddRange(groups),
+            _targetAll);
+    }
+
+    public readonly SendManager<TResponse> All => new(_manager, _connectionIds, _groups, true);
 
     public readonly ValueTask SendAsync(TResponse data, CancellationToken token)
     {
