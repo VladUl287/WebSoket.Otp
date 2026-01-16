9ca4d343d83f832956bcf417187c97cd67b03572
User <ulyanovskiy.2001@mail.ru>
2026-01-16 22:02:55 +0300

    feat: make send manager builder is accumulate data in one instance instead of create new each time

--- WebSocket.Otp.Abstractions/SendManager.cs
+++ WebSocket.Otp.Abstractions/SendManager.cs
@@ -1,48 +1,50 @@
-﻿using System.Collections.Immutable;
-using WebSockets.Otp.Abstractions.Contracts;
+﻿using WebSockets.Otp.Abstractions.Contracts;
 
 namespace WebSockets.Otp.Abstractions;
 
-public sealed record SendManager(
-   IWsConnectionManager Manager,
-   ImmutableArray<string> ConnectionIds,
-   ImmutableArray<string> Groups,
-   bool TargetAll)
+public sealed class SendManager(IWsConnectionManager manager)
 {
-    private readonly IWsConnectionManager _manager = Manager;
-    private readonly ImmutableArray<string> _connectionIds = ConnectionIds;
-    private readonly ImmutableArray<string> _groups = Groups;
-    private readonly bool _targetAll = TargetAll;
+    private readonly IWsConnectionManager _manager = manager;
+    private readonly HashSet<string> _connectionIds = [];
+    private readonly HashSet<string> _groups = [];
+    private bool _targetAll = false;
 
     public SendManager Client(string connectionId)
     {
         if (_targetAll) return this;
-
-        return new(_manager, _connectionIds.Add(connectionId), _groups, _targetAll);
+        _connectionIds.Add(connectionId);
+        return this;
     }
 
     public SendManager Group(string groupName)
     {
         if (_targetAll) return this;
-
-        return new(_manager, _connectionIds, _groups.Add(groupName), _targetAll);
+        _groups.Add(groupName);
+        return this;
     }
 
-    public SendManager All => new(_manager, _connectionIds, _groups, true);
+    public SendManager All
+    {
+        get
+        {
+            _targetAll = true;
+            return this;
+        }
+    }
 
     public ValueTask SendAsync<TResponse>(TResponse data, CancellationToken token)
     {
-        return ValueTask.CompletedTask;
+        return _manager.AddToGroupAsync(string.Empty, string.Empty);
     }
 }
 
-public sealed record SendManager<TResponse>(SendManager Manager) 
+public sealed class SendManager<TResponse>(SendManager Manager)
     where TResponse : notnull
 {
     public SendManager<TResponse> Client(string connectionId) =>
         new(Manager.Client(connectionId));
 
-    public SendManager<TResponse> Group(string groupName) => 
+    public SendManager<TResponse> Group(string groupName) =>
         new(Manager.Group(groupName));
 
     public SendManager<TResponse> All => new(Manager.All);
