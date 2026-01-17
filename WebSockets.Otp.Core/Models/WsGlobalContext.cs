ab957c1309f1eb1bb813f5e0d443f1d652f5cbcd
User <ulyanovskiy.2001@mail.ru>
2026-01-17 13:53:19 +0300

    feat: implement global context

---
+++ WebSockets.Otp.Core/Models/WsGlobalContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using WebSockets.Otp.Abstractions;
+using WebSockets.Otp.Abstractions.Contracts;
+using WebSockets.Otp.Abstractions.Endpoints;
+
+namespace WebSockets.Otp.Core.Models;
+
+public sealed class WsGlobalContext : IGlobalContext
+{
+    private readonly IWsConnectionManager _connectionManager;
+
+    public WsGlobalContext(HttpContext httpContext, string connectionId, IWsConnectionManager manager)
+    {
+        _connectionManager = manager;
+
+        Context = httpContext;
+        ConnectionId = connectionId;
+    }
+
+    public HttpContext Context { get; init; }
+
+    public string ConnectionId { get; init; }
+
+    public GroupManager Groups => new(_connectionManager);
+}
