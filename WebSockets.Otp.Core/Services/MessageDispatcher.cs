f41584670a3a0cd0269af7be4c0f78a23a33ad07
User <ulyanovskiy.2001@mail.ru>
2026-01-18 15:36:15 +0300

    refactor: message dispatcher

--- WebSockets.Otp.Core/Services/MessageDispatcher.cs
+++ WebSockets.Otp.Core/Services/MessageDispatcher.cs
@@ -8,24 +8,34 @@ using WebSockets.Otp.Core.Utils;
 namespace WebSockets.Otp.Core.Services;
 
 public class MessageDispatcher(
-    IServiceScopeFactory scopeFactory, IWsConnectionManager connectionManager, IExecutionContextFactory contextFactory, 
+    IServiceScopeFactory scopeFactory, IWsConnectionManager connectionManager, IExecutionContextFactory contextFactory,
     IPipelineFactory pipelineFactory, IStringPool stringPool) : IMessageDispatcher
 {
-    private readonly ReadOnlyMemory<byte> Key = stringPool.Encoding.GetBytes(WsMessageFields.Key).AsMemory();
+    private readonly ReadOnlyMemory<byte> _endpointKeyBytes = stringPool.Encoding.GetBytes(WsMessageFields.Key).AsMemory();
 
     public async Task DispatchMessage(
         IGlobalContext globalContext, ISerializer serializer, IMessageBuffer payload, CancellationToken token)
     {
-        var endpointKey = serializer.ExtractField(Key.Span, payload.Span, stringPool);
+        var endpointKey = serializer.ExtractField(
+            _endpointKeyBytes.Span,
+            payload.Span,
+            stringPool
+        );
 
         await using var scope = scopeFactory.CreateAsyncScope();
 
         var endpoint = scope.ServiceProvider.GetRequiredKeyedService(typeof(IWsEndpoint), endpointKey);
 
-        var execCtx = contextFactory.Create(globalContext, connectionManager, payload, serializer, token);
+        var execCtx = contextFactory.Create(
+            globalContext,
+            connectionManager,
+            payload,
+            serializer,
+            token);
 
         var endpointType = endpoint.GetType();
         var pipeline = pipelineFactory.CreatePipeline(endpointType);
+
         await pipeline.ExecuteAsync(endpoint, execCtx);
     }
 }
