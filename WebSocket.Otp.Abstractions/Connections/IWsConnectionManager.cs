471a3ee57e3848df246cb22274b321305d471b88
User <ulyanovskiy.2001@mail.ru>
2026-01-20 17:23:39 +0300

    feat: implement inmemory connection manager

--- WebSocket.Otp.Abstractions/Connections/IWsConnectionManager.cs
+++ WebSocket.Otp.Abstractions/Connections/IWsConnectionManager.cs
@@ -5,8 +5,8 @@ public interface IWsConnectionManager
     bool TryAdd(IWsConnection connection);
     bool TryRemove(string connectionId);
 
-    ValueTask AddToGroupAsync(string group, string connectionId);
-    ValueTask RemoveFromGroupAsync(string group, string connectionId);
+    ValueTask<bool> AddToGroupAsync(string group, string connectionId);
+    ValueTask<bool> RemoveFromGroupAsync(string group, string connectionId);
 
     ValueTask SendAsync<TData>(string connectionId, TData data, CancellationToken token)
         where TData : notnull;
--- WebSockets.Otp.Core/Services/InMemoryConnectionManager.cs
+++ WebSockets.Otp.Core/Services/InMemoryConnectionManager.cs
@@ -6,37 +6,47 @@ namespace WebSockets.Otp.Core.Services;
 public sealed class InMemoryConnectionManager : IWsConnectionManager
 {
     private readonly ConcurrentDictionary<string, IWsConnection> _store = new();
+    private readonly ConcurrentDictionary<string, HashSet<string>> _groups = new();
 
     public bool TryAdd(IWsConnection connection) => _store.TryAdd(connection.Id, connection);
-
     public bool TryRemove(string connectionId) => _store.TryRemove(connectionId, out _);
 
-    public ValueTask AddToGroupAsync(string group, string connectionId)
+    public ValueTask<bool> AddToGroupAsync(string group, string connectionId)
+    {
+        var added = _groups
+            .GetOrAdd(group, [])
+            .Add(group);
+        return ValueTask.FromResult(added);
+    }
+
+    public ValueTask<bool> RemoveFromGroupAsync(string group, string connectionId)
+    {
+        var removed = _groups
+            .GetOrAdd(group, [])
+            .Remove(group);
+        return ValueTask.FromResult(removed);
+    }
+
+    public ValueTask SendAsync<TData>(string connectionId, TData data, CancellationToken token)
+        where TData : notnull
+    {
+        return _store[connectionId].Transport.SendAsync(data, token);
+    }
+
+    public ValueTask SendAsync<TData>(IEnumerable<string> connections, TData data, CancellationToken token)
+        where TData : notnull
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask RemoveFromGroupAsync(string group, string connectionId)
+    public ValueTask SendToGroupAsync<TData>(string group, TData data, CancellationToken token)
+        where TData : notnull
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask SendAsync<TData>(string connectionId, TData data, CancellationToken token) where TData : notnull
-    {
-        throw new NotImplementedException();
-    }
-
-    public ValueTask SendAsync<TData>(IEnumerable<string> connections, TData data, CancellationToken token) where TData : notnull
-    {
-        throw new NotImplementedException();
-    }
-
-    public ValueTask SendToGroupAsync<TData>(string group, TData data, CancellationToken token) where TData : notnull
-    {
-        throw new NotImplementedException();
-    }
-
-    public ValueTask SendToGroupAsync<TData>(IEnumerable<string> groups, TData data, CancellationToken token) where TData : notnull
+    public ValueTask SendToGroupAsync<TData>(IEnumerable<string> groups, TData data, CancellationToken token)
+        where TData : notnull
     {
         throw new NotImplementedException();
     }
