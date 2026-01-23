f897261bd85c657753de219deecc7eaf348604fa
User <ulyanovskiy.2001@mail.ru>
2026-01-23 18:04:36 +0300

    refactor: handshake options

--- WebSocket.Otp.Abstractions/Options/WsHandshakeOptions.cs
+++ WebSocket.Otp.Abstractions/Options/WsHandshakeOptions.cs
@@ -2,9 +2,5 @@
 
 public sealed class WsHandshakeOptions
 {
-    public string Protocol { get; set; } = "json";
-
-    //public TimeSpan KeepAliveInterval { get; set; } = TimeSpan.FromMinutes(2);
-    //public int MaxConnections { get; set; } = 1000;
-    //public int MaxConnectionsPerUser { get; set; } = 10;
+    public string Protocol { get; init; } = "json";
 }
