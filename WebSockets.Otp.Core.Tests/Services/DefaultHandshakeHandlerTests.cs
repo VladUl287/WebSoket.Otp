7d02d7d64b41d7c56cf45622795e9dde026aaff3
User <ulyanovskiy.2001@mail.ru>
2026-01-26 16:15:09 +0300

    tests: update connection handler and hanshake handler tests

--- WebSockets.Otp.Core.Tests/Services/DefaultHandshakeHandlerTests.cs
+++ WebSockets.Otp.Core.Tests/Services/DefaultHandshakeHandlerTests.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using System.Buffers;
 using System.Net.WebSockets;
 using WebSockets.Otp.Abstractions.Options;
 using WebSockets.Otp.Abstractions.Serializers;
 using WebSockets.Otp.Abstractions.Transport;
 using WebSockets.Otp.Abstractions.Utils;
 using WebSockets.Otp.Core.Services;
+using WebSockets.Otp.Core.Services.Serializers;
 
 namespace WebSockets.Otp.Core.Tests.Services;
 
@@ -90,211 +92,6 @@ public class DefaultHandshakeHandlerTests
     }
 
     [Fact]
-    public async Task HandleAsync_ReturnsNull_WhenDeserializationFails()
-    {
-        // Arrange
-        var options = new WsConfiguration(new WsOptions());
-        var token = CancellationToken.None;
-        var bufferMock = new Mock<IMessageBuffer>();
-        var serializerMock = new Mock<ISerializer>();
-
-        // Setup buffer
-        var bufferData = new byte[] { 1, 2, 3 };
-        bufferMock.SetupGet(b => b.Length).Returns(bufferData.Length);
-
-        var messageBuffer = bufferMock.Object;
-        var enumerable = new[] { messageBuffer }.ToAsyncEnumerable();
-
-        _enumeratorMock
-            .Setup(e => e.EnumerateAsync(_webSocketMock.Object, options, _objectPoolMock.Object, token))
-            .Returns(enumerable);
-
-        _storeMock
-            .Setup(s => s.TryGet("json", out It.Ref<ISerializer?>.IsAny))
-            .Callback((string protocol, out ISerializer? serializer) =>
-            {
-                serializer = serializerMock.Object;
-            })
-            .Returns(true);
-
-        serializerMock
-            .Setup(s => s.Deserialize(typeof(WsHandshakeOptions), bufferData))
-            .Returns(null);
-
-        // Act
-        var result = await _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token);
-
-        // Assert
-        Assert.Null(result);
-        serializerMock.Verify(s => s.Deserialize(typeof(WsHandshakeOptions), bufferData), Times.Once);
-    }
-
-    [Fact]
-    public async Task HandleAsync_ReturnsHandshakeOptions_WhenSuccessful()
-    {
-        // Arrange
-        var options = new WsConfiguration(new WsOptions());
-        var token = CancellationToken.None;
-        var bufferMock = new Mock<IMessageBuffer>();
-        var serializerMock = new Mock<ISerializer>();
-        var expectedHandshakeOptions = new WsHandshakeOptions();
-
-        // Setup buffer
-        var bufferData = "{}"u8.ToArray();
-        bufferMock.SetupGet(b => b.Length).Returns(bufferData.Length);
-
-        // Setup disposal
-        bufferMock.Setup(b => b.Dispose()).Verifiable();
-
-        var messageBuffer = bufferMock.Object;
-        var enumerable = new[] { messageBuffer }.ToAsyncEnumerable();
-
-        _enumeratorMock
-            .Setup(e => e.EnumerateAsync(_webSocketMock.Object, options, _objectPoolMock.Object, token))
-            .Returns(enumerable);
-
-        _storeMock
-            .Setup(s => s.TryGet("json", out It.Ref<ISerializer?>.IsAny))
-            .Callback((string protocol, out ISerializer? serializer) =>
-            {
-                serializer = serializerMock.Object;
-            })
-            .Returns(true);
-
-        serializerMock
-            .Setup(s => s.Deserialize(typeof(WsHandshakeOptions), bufferData))
-            .Returns(expectedHandshakeOptions);
-
-        var qa = "{}"u8.ToArray();
-        _webSocketMock
-            .Setup(s => s.SendAsync(
-                It.Is<ReadOnlyMemory<byte>>(m => m.ToArray().SequenceEqual(qa)),
-                WebSocketMessageType.Text,
-                true,
-                token
-            ))
-            .Returns(ValueTask.CompletedTask)
-            .Verifiable();
-
-        // Act
-        var result = await _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token);
-
-        // Assert
-        Assert.Same(expectedHandshakeOptions, result);
-        bufferMock.Verify(b => b.Dispose(), Times.Once);
-        _webSocketMock.Verify();
-    }
-
-    [Fact]
-    public async Task HandleAsync_SendsResponse_WhenHandshakeSuccessful()
-    {
-        // Arrange
-        var options = new WsConfiguration(new WsOptions());
-        var token = CancellationToken.None;
-        var bufferMock = new Mock<IMessageBuffer>();
-        var serializerMock = new Mock<ISerializer>();
-        var expectedHandshakeOptions = new WsHandshakeOptions();
-
-        // Setup buffer
-        var bufferData = "{}"u8.ToArray();
-        bufferMock.SetupGet(b => b.Length).Returns(bufferData.Length);
-        //bufferMock.SetupGet(b => b.Span).Returns(bufferData.AsSpan());
-
-        var messageBuffer = bufferMock.Object;
-        var enumerable = new[] { messageBuffer }.ToAsyncEnumerable();
-
-        _enumeratorMock
-            .Setup(e => e.EnumerateAsync(_webSocketMock.Object, options, _objectPoolMock.Object, token))
-            .Returns(enumerable);
-
-        _storeMock
-            .Setup(s => s.TryGet("json", out It.Ref<ISerializer?>.IsAny))
-            .Callback((string protocol, out ISerializer? serializer) =>
-            {
-                serializer = serializerMock.Object;
-            })
-            .Returns(true);
-
-        serializerMock
-            .Setup(s => s.Deserialize(typeof(WsHandshakeOptions), bufferData))
-            .Returns(expectedHandshakeOptions);
-
-        var qa = "{}"u8.ToArray();
-        var sendCalled = false;
-        _webSocketMock
-            .Setup(s => s.SendAsync(
-                It.Is<ReadOnlyMemory<byte>>(m => m.ToArray().SequenceEqual(qa)),
-                WebSocketMessageType.Text,
-                true,
-                token
-            ))
-            .Callback(() => sendCalled = true)
-            .Returns(ValueTask.CompletedTask);
-
-        // Act
-        var result = await _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token);
-
-        // Assert
-        Assert.True(sendCalled);
-        _webSocketMock.Verify(s => s.SendAsync(
-            It.Is<ReadOnlyMemory<byte>>(m => m.ToArray().SequenceEqual(qa)),
-            WebSocketMessageType.Text,
-            true,
-            token
-        ), Times.Once);
-    }
-
-    [Fact]
-    public async Task HandleAsync_DisposesBuffer_AfterProcessing()
-    {
-        // Arrange
-        var options = new WsConfiguration(new WsOptions());
-        var token = CancellationToken.None;
-        var bufferMock = new Mock<IMessageBuffer>();
-        var serializerMock = new Mock<ISerializer>();
-        var expectedHandshakeOptions = new WsHandshakeOptions();
-
-        // Setup buffer
-        var bufferData = "{}"u8.ToArray();
-        bufferMock.SetupGet(b => b.Length).Returns(bufferData.Length);
-        //bufferMock.SetupGet(b => b.Span).Returns(bufferData.AsSpan());
-
-        // Track disposal
-        var disposed = false;
-        bufferMock.Setup(b => b.Dispose()).Callback(() => disposed = true);
-
-        var messageBuffer = bufferMock.Object;
-        var enumerable = new[] { messageBuffer }.ToAsyncEnumerable();
-
-        _enumeratorMock
-            .Setup(e => e.EnumerateAsync(_webSocketMock.Object, options, _objectPoolMock.Object, token))
-            .Returns(enumerable);
-
-        _storeMock
-            .Setup(s => s.TryGet("json", out It.Ref<ISerializer?>.IsAny))
-            .Callback((string protocol, out ISerializer? serializer) =>
-            {
-                serializer = serializerMock.Object;
-            })
-            .Returns(true);
-
-        serializerMock
-            .Setup(s => s.Deserialize(typeof(WsHandshakeOptions), bufferData))
-            .Returns(expectedHandshakeOptions);
-
-        _webSocketMock
-            .Setup(s => s.SendAsync(It.IsAny<ReadOnlyMemory<byte>>(), WebSocketMessageType.Text, true, token))
-            .Returns(ValueTask.CompletedTask);
-
-        // Act
-        await _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token);
-
-        // Assert
-        Assert.True(disposed);
-        bufferMock.Verify(b => b.Dispose(), Times.Once);
-    }
-
-    [Fact]
     public async Task HandleAsync_ThrowsOperationCanceledException_WhenCancelled()
     {
         // Arrange
@@ -312,43 +109,6 @@ public class DefaultHandshakeHandlerTests
         await Assert.ThrowsAsync<OperationCanceledException>(() =>
             _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token).AsTask()
         );
-    }
-
-    [Fact]
-    public async Task HandleAsync_ReturnsNull_WhenSerializerReturnsNull()
-    {
-        // Arrange
-        var options = new WsConfiguration(new WsOptions());
-        var token = CancellationToken.None;
-        var bufferMock = new Mock<IMessageBuffer>();
-        var serializerMock = new Mock<ISerializer>();
-
-        // Setup buffer
-        var bufferData = "{}"u8.ToArray();
-        bufferMock.SetupGet(b => b.Length).Returns(bufferData.Length);
-        //bufferMock.SetupGet(b => b.Span).Returns(bufferData.AsSpan());
-
-        var messageBuffer = bufferMock.Object;
-        var enumerable = new[] { messageBuffer }.ToAsyncEnumerable();
-
-        _enumeratorMock
-            .Setup(e => e.EnumerateAsync(_webSocketMock.Object, options, _objectPoolMock.Object, token))
-            .Returns(enumerable);
-
-        ISerializer? serializer = serializerMock.Object;
-        _storeMock
-            .Setup(s => s.TryGet("json", out serializer))
-            .Returns(true);
-
-        serializerMock
-            .Setup(s => s.Deserialize(It.IsAny<Type>(), bufferData))
-            .Returns<object?>(null);
-
-        // Act
-        var result = await _handler.HandleAsync(_httpContextMock.Object, _webSocketMock.Object, options, token);
-
-        // Assert
-        Assert.Null(result);
     }
 }
 
