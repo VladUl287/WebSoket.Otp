7387b019209179ebd63f8097e2446fdff851758c
User <ulyanovskiy.2001@mail.ru>
2026-02-20 11:08:10 +0300

    feat: js client improve endpoints client

--- WebSockets.Otp.Js/websocket-client.ts
+++ WebSockets.Otp.Js/websocket-client.ts
@@ -1,32 +1,78 @@
 import { WsMessage, HandshakeOptions } from "./types"
 
-export function useWsEndpoints(webSocket: WebSocket) {
+export type WsMultiplexingOptions = {
+    // mode: 'round-robin' | 'least-loaded' | 'parallel'
+    connectionsCount: number
+    // minConnections: number
+    // maxConnections: number
+}
+
+export type WsOptions = {
+    url: string | URL
+    protocols?: string | string[]
+    multiplexing?: WsMultiplexingOptions
+}
+
+export function useWsEndpoints(options: WsOptions) {
+    const sockets: WebSocket[] = []
     const listeners = new Map<string, (message: any) => any>()
 
-    webSocket.onmessage = (event) => {
-        const data = JSON.parse(event.data) as WsMessage
-        const listener = listeners.get(data.key)
-        listener && listener(data)
+    const initConnections = (options: WsOptions) => {
+        const connectionsCount = Math.max(options.multiplexing?.connectionsCount ?? 1, 1)
+
+        for (let i = 0; i < connectionsCount; i++) {
+            const socket = new WebSocket(options.url, options.protocols);
+
+            socket.onmessage = (event) => {
+                const data = JSON.parse(event.data) as WsMessage
+                const listener = listeners.get(data.key)
+                listener && listener(data)
+            }
+
+            sockets.push(socket)
+        }
+    }
+
+    initConnections(options)
+
+    let currentConnectionIndex = 0
+
+    const getNextConnection = (options: WsOptions) => {
+        const connection = sockets[currentConnectionIndex]
+        currentConnectionIndex = (currentConnectionIndex + 1) % (options.multiplexing?.connectionsCount ?? 1)
+        return connection
     }
 
     const connect = (options: HandshakeOptions) => {
-        webSocket.send(JSON.stringify(options))
+        sockets.forEach((socket) => {
+            socket.send(JSON.stringify(options))
+        })
     }
 
     const send = <T>(key: string, message: T) => {
+        const webSocket = getNextConnection(options)
         webSocket.send(JSON.stringify({
             key,
             ...message
         }))
     }
 
+    // const request = <T, TResult>(key: string, message: T): Promise<TResult> => {}
+
     const receive = <T>(key: string, callback: (message: T) => any) => {
         listeners.set(key, callback)
+    }
+
+    const disconnect = () => {
+        sockets.forEach((socket) => {
+            socket.close()
+        })
     }
 
     return {
         connect,
         send,
-        receive
+        receive,
+        disconnect
     }
 }
