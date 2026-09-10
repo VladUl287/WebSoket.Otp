0dc4ed0438c42e5a79e25cb7c8caf954d9108f27
User <ulyanovskiy.2001@mail.ru>
2026-09-10 22:10:53 +0300

    feat: initilize trie resolver

---
+++ WebSockets.Otp.Core/Utils/TrieResolver.cs
@@ -0,0 +1,16 @@
+﻿using WebSockets.Otp.Abstractions.Utils;
+
+namespace WebSockets.Otp.Core.Utils;
+
+public sealed class TrieResolver<T> : ITrieResolver<T>
+{
+    public T Resolve(byte[] sequence)
+    {
+        return Resolve(sequence.AsSpan());
+    }
+
+    public T Resolve(ReadOnlySpan<byte> sequence)
+    {
+        throw new NotImplementedException();
+    }
+}
