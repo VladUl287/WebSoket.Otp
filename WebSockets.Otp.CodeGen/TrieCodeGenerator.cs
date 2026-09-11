fcfb63be04999476e86dd0d391ac0fff92b06584
User <ulyanovskiy.2001@mail.ru>
2026-09-11 16:01:30 +0300

    feat: intilize code gen project for resolve endpoint

---
+++ WebSockets.Otp.CodeGen/TrieCodeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace WebSockets.Otp.CodeGen
+{
+    [Generator]
+    public class TrieCodeGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                //var trie = CSharpTrieCodeGenerator.GenerateTrie((fieldsBytes));
+
+                //ctx.AddSource("TrieResolver.g.cs", SourceText.From(trie, Encoding.UTF8));
+            });
+        }
+    }
+}
