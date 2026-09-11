996d5b540833ac4f1a4eb4b4383ce3e3ac1f305a
User <ulyanovskiy.2001@mail.ru>
2026-09-11 15:33:10 +0300

    feat: create csharp code compiler

---
+++ WebSockets.Otp.Core/Utils/CSharpCompiler.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace WebSockets.Otp.Core.Utils;
+
+public class CSharpCompiler
+{
+    public static Assembly Compile(string source, MetadataReference[] references)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+        var compilation = CSharpCompilation.Create(
+            "GeneratedAssembly", [syntaxTree], references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        if (!result.Success)
+        {
+            var errors = result.Diagnostics
+                .Where(d => d.Severity == DiagnosticSeverity.Error)
+                .Select(d => d.ToString());
+            throw new InvalidOperationException("Compilation failed:\n" + string.Join("\n", errors));
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+        return Assembly.Load(ms.ToArray());
+    }
+}
