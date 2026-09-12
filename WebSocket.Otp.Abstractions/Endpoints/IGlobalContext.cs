4dea898915bd0c9dc0b320ebb2280b4e8b19d1a1
User <ulyanovskiy.2001@mail.ru>
2026-09-12 22:21:31 +0300

    feat: move snapshot options to global context

--- WebSocket.Otp.Abstractions/Endpoints/IGlobalContext.cs
+++ WebSocket.Otp.Abstractions/Endpoints/IGlobalContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Net.WebSockets;
+using WebSockets.Otp.Abstractions.Options;
 
 namespace WebSockets.Otp.Abstractions.Endpoints;
 
@@ -7,6 +8,7 @@ public interface IGlobalContext
 {
     HttpContext Context { get; }
     WebSocket Socket { get; }
+    WsOptionsSnapshot Options { get; }
     string ConnectionId { get; }
     GroupManager Groups { get; }
 }
