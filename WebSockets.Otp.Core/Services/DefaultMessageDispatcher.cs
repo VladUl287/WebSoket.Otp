703207d661202ff4dfe90c1c08994756781ffbc5
User <ulyanovskiy.2001@mail.ru>
2026-09-12 21:38:16 +0300

    feat: add basic auth

--- WebSockets.Otp.Core/Services/DefaultMessageDispatcher.cs
+++ WebSockets.Otp.Core/Services/DefaultMessageDispatcher.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using WebSockets.Otp.Abstractions.Connections;
 using WebSockets.Otp.Abstractions.Contracts;
 using WebSockets.Otp.Abstractions.Endpoints;
+using WebSockets.Otp.Abstractions.Options;
 using WebSockets.Otp.Abstractions.Serializers;
 using WebSockets.Otp.Abstractions.Transport;
 using WebSockets.Otp.Abstractions.Utils;
@@ -12,12 +17,12 @@ namespace WebSockets.Otp.Core.Services;
 
 public class DefaultMessageDispatcher(
     IServiceScopeFactory scopeFactory, IWsConnectionManager connectionManager, IContextFactory contextFactory,
-    IEndpointInvoker invoker, ITrieResolver endpointTypeResolver) : IMessageDispatcher
+    IEndpointInvokerFactory invokerFactory, ITrieResolver endpointTypeResolver) : IMessageDispatcher
 {
     private readonly ReadOnlyMemory<byte> _endpointKeyBytes = Encoding.UTF8.GetBytes(WsMessageFields.Key).AsMemory();
 
     public async Task DispatchMessage(
-        IGlobalContext globalContext, ISerializer serializer, IMessageBuffer payload, CancellationToken token)
+        IGlobalContext globalContext, ISerializer serializer, IMessageBuffer payload, WsConfiguration configuration, CancellationToken token)
     {
         var keyIndex = serializer.FieldIndex(payload.Span, _endpointKeyBytes.Span);
 
@@ -38,6 +43,31 @@ public class DefaultMessageDispatcher(
 
         var execCtx = contextFactory.Create(globalContext, connectionManager, payload, serializer, token);
 
+        var source = globalContext.Context;
+        var ctx = new DefaultHttpContext
+        {
+            RequestServices = scope.ServiceProvider,
+            User = source.User,
+            RequestAborted = token,
+            Items = source.Items
+        };
+
+        var attribute = endpointType.GetCustomAttribute<AuthorizeAttribute>() ??
+              throw new InvalidOperationException($"Type {endpointType.Name} is missing WsEndpointAttribute");
+        
+        ctx.SetEndpoint(new Endpoint(
+            requestDelegate: null,
+            metadata: new EndpointMetadataCollection(attribute),
+            displayName: "ws-auth"));
+
+        await configuration.AuthPipeline(ctx);
+
+        if (ctx.Response.StatusCode is StatusCodes.Status401Unauthorized or StatusCodes.Status403Forbidden)
+        {
+            return;
+        }
+
+        var invoker = invokerFactory.Create(endpointType);
         await invoker.Invoke(endpoint, execCtx);
     }
 }
