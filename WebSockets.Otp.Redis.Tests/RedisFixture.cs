3e020f1d4878d8188c219e6feca0f73c68a88b93
User <ulyanovskiy.2001@mail.ru>
2026-09-18 17:16:53 +0300

    tests: add redist containers for tests

---
+++ WebSockets.Otp.Redis.Tests/RedisFixture.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using Testcontainers.Redis;
+
+namespace WebSockets.Otp.Redis.Tests;
+
+public sealed class RedisFixture : IAsyncLifetime
+{
+    private RedisContainer _container = null!;
+    public IConnectionMultiplexer Multiplexer { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        _container = new RedisBuilder("redis:7-alpine").Build();
+
+        await _container.StartAsync();
+
+        var options = ConfigurationOptions.Parse(_container.GetConnectionString());
+        options.AllowAdmin = true;
+        Multiplexer = await ConnectionMultiplexer.ConnectAsync(options);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Multiplexer.DisposeAsync();
+        await _container.DisposeAsync();
+    }
+
+    public async Task FlushAsync()
+    {
+        foreach (var ep in Multiplexer.GetEndPoints())
+        {
+            var server = Multiplexer.GetServer(ep);
+            await server.FlushDatabaseAsync();
+        }
+    }
+}
+
+[CollectionDefinition("redis")]
+public sealed class RedisCollection : ICollectionFixture<RedisFixture> { }
